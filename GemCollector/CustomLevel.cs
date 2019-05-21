0b24f089a837cec1493dafe09ae92492af831515
jamasouk354 <jamasouk354@SCS-D-360QJ2>
2019-05-21 09:28:51 -0400

    YO

--- GemCollector/CustomLevel.cs
+++ GemCollector/CustomLevel.cs
@@ -23,9 +23,23 @@ namespace GemCollector
 
         private void startButton_Click(object sender, EventArgs e)
         {
+            int total;
             SelectScreen.GridHeight = Convert.ToInt32(heightInput.Text);
+            if (SelectScreen.GridHeight < 4)
+            {
+                outputLabel.Text = "Height is too low";
+            }
             SelectScreen.GridWidth = Convert.ToInt32(widthInput.Text);
+            if (SelectScreen.GridWidth < 4)
+            {
+                outputLabel.Text = "Width is too low";
+            }
             SelectScreen.GemNum = Convert.ToInt32(gemInput.Text);
+            total = SelectScreen.GridHeight * SelectScreen.GridWidth;
+            if (SelectScreen.GemNum > total)
+            {
+                outputLabel.Text = "Too much gems";
+            }
 
             GameScreen gs = new GameScreen();
             Form f = this.FindForm();
